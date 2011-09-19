e34de2ca1eab8dbfc00a4aff1d76e58cc4e6f781
Brett <brettatoms@gmail.com>
2011-09-18 21:03:55 -0400

    - updated bzr command and fixed expanding the home directory

--- install-dvc.sh
+++ install-dvc.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-EMACS_DIR="~/.emacs.d/"
+EMACS_DIR="$HOME/.emacs.d"
 PACKAGE_DIR="$EMACS_DIR/packages"
 DVC_DEV_DIR="$PACKAGE_DIR/dvc-dev"
 BUILD_DIR="$DVC_DEV_DIR/++build"
 
 # TODO: if DVC_DEV_DIR already exists then try to just pull recent
 # updates
-bzr get http://bzr.xsteve.at/dvc/ $DVC_DEV_DIR
+#mkdir -p $BUILD_DIR
+bzr branch http://bzr.xsteve.at/dvc/ $DVC_DEV_DIR
 cd $DVC_DEV_DIR
 autoconf
 mkdir -p $BUILD_DIR
