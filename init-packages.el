dadd74a9d18266f02cf7f3c109045beab0ab7338
Brett <brettatoms@gmail.com>
2013-06-10 10:14:04 -0400

    - use js-mode in multi-web-mode

--- init-packages.el
+++ init-packages.el
@@ -32,12 +32,13 @@
 ;; multi-web-mode
 ;;
 (when (package-installed-p 'multi-web-mode)
-  (setq mweb-default-major-mode 'html-mode)
-  (setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
-		    (js2-mode "<script +\\(type=\"text/javascript\"\\|language=\"javascript\"\\)[^>]*>" "</script>")
-		    (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
-  (setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5"))
-  ;(multi-web-global-mode 1)
+   
+   (setq mweb-default-major-mode 'html-mode)
+   (setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
+                      (js-mode "<script +\\(type=\"text/javascript\"\\|language=\"javascript\"\\)[^>]*>" "</script>")
+                      (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
+   (setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5"))
+   ;(multi-web-global-mode 1)
 )
 
 ;;
